9320f3ac8f0a0164f17b23526e2a4e4bf60961c3
Matth-ewe-f <matthewtf73@gmail.com>
2024-07-06 22:47:46 -0400

    fixed bad require

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,7 @@
 // server/index.ts
 require('dotenv').config();
 const PORT = process.env.PORT;
-const App = require('./app.ts');
+const App = require('./app');
 App.dbConnection();
 const app = App.app;
 
